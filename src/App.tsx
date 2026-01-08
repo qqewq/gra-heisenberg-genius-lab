9311e2428698e5de889e7061f464b66c4f38747a
oleg bits <bits.oleg@bk.ru>
2026-01-08 16:33:18 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-// src/App.tsx
 import { SimulatorUI } from '@/components/SimulatorUI';
 
-function App() {
+export default function App() {
   return (
     <div className="container mx-auto p-4 max-w-4xl py-8">
       <SimulatorUI />
     </div>
   );
 }
-
-export default App;
