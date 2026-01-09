22652c5fb413dded4936b933dd2d75ef404cfe94
oleg bits <bits.oleg@bk.ru>
2026-01-09 17:55:35 +0300

    Create index.ts

---
+++ src/lib/quantum/index.ts
@@ -0,0 +1,3 @@
+export { default as HubbardCalculator } from './hubbard-calculator';
+export type { HubbardResult } from './hubbard-calculator';
+export { generateHubbardTestData, validateHubbardResults } from './hubbard-calculator';
